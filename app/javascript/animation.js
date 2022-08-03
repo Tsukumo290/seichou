09a2c6d76ec987b519da2c8747ab68a03eec8412
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-03 20:58:22 +0900

    レイアウトの調整

--- app/javascript/animation.js
+++ app/javascript/animation.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function(){
-lottie.loadAnimation({
-  container: document.getElementById('animation'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/animations/41351-baby-mom-love.json'
-});
+  lottie.loadAnimation({
+    container: document.getElementById('animation'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '/animations/41351-baby-mom-love.json'
+  });
 })
