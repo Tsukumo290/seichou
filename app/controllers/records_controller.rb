638d1dc6a990728c6e6f258b11526d01444ed751
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-03 20:04:59 +0900

    ホームページの作成

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,7 @@
 class RecordsController < ApplicationController
   def home
+    
+  end
+  def index
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+ require("../animation")
+ require("../app")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
