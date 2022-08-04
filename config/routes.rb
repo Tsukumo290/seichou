25ea6bf9120cd25d4508493e9056fb298a773eee
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-04 17:03:03 +0900

    new.html.erbのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "records#home"
-  resources :records, only: [:index, :new, :create, :show]
+  resources :records, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:edit, :update]
 end
