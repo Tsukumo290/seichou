bf3c3d2171fc6a9be6fb2c201d8054acc17fc4ee
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-05 23:40:16 +0900

    editのプレビュー表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_post');
+  const postForm = document.getElementById('record_post');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function(){
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
     previewWrapper.appendChild(previewImage);
-    previewList.appendChild(previewWrapper)
+    previewList.appendChild(previewWrapper);
     previewNone.style.display = 'none';
   });
 });
