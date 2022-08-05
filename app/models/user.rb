385ccedde1fae5313d04887183d049597124843c
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-05 20:09:55 +0900

    recordsのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 10 }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   
   has_many :records
 end
