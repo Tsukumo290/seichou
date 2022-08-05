826f16f167e6cd30775156e92aae4f75d48c6c3d
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-05 11:28:59 +0900

    ユーザーページの調整

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,5 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!, except: :home
 
   def home
   end
--- app/javascript/app.js
+++ app/javascript/app.js
@@ -16,4 +16,23 @@ window.addEventListener('load', function(){
     circle.style.transform = rotateSum;
     rotateValue = rotateSum;
   };
-})
+});
+window.addEventListener('load', function(){
+  var circle = document.getElementById("circle");
+  var upBtn = document.getElementById("upBtn");
+  var downBtn = document.getElementById("downBtn");
+
+  var rotateValue = circle.style.transform;
+  var rotateSum;
+
+  upBtn.onclick = function() {
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+  };
+  downBtn.onclick = function() {
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+  };
+});
