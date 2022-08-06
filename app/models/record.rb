8a7caa7bc5ddb404657cd2f1af871dbfddfb6ffb
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-06 16:46:05 +0900

    バリデーションのまとめ

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,9 +2,11 @@ class Record < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image,   presence: true
-  validates :title,   presence: true, length: { maximum: 10 }
-  validates :content, presence: true, length: { maximum: 200 }
+  with_options presence: true do
+    validates :image
+    validates :title, length: { maximum: 10 }
+    validates :content, length: { maximum: 200 }
+  end
   validates :height, allow_blank: true,  numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999}
   validates :weight, allow_blank: true,  numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9999}
 end
