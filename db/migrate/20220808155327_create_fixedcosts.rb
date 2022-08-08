102ba19d9485f3dc005fed66de628c7b707f1274
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-09 01:03:03 +0900

    fixedcostsテーブルの作成

--- db/migrate/20220808155327_create_fixedcosts.rb
+++ db/migrate/20220808155327_create_fixedcosts.rb
@@ -1,7 +1,8 @@
 class CreateFixedcosts < ActiveRecord::Migration[6.0]
   def change
     create_table :fixedcosts do |t|
-
+      t.string :name, null: false
+      t.string :description
       t.timestamps
     end
   end
